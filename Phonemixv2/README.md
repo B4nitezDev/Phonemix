5e8508a1ecd45be04d34f5c7112465819a4472d8
b4nitezdev <benitezdev01@gmail.com>
2024-07-22 17:56:04 -0300

    borro modelo

--- Phonemixv2/README.md
+++
@@ -1,1 +0,0 @@
-Welcome to Phonemix, the Pronunciation Feedback API.
--- Phonemixv2/config/config.py
+++
@@ -1,8 +0,0 @@
-import yaml
-
-class Config:
-    def __init__(self, config_file):
-        with open(config_file, 'r') as file:
-            self.config = yaml.safe_load(file)
-            
-phonemize_config = Config('config/phonemize_config.yml').config
