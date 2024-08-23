785df7d613dfe94d4676595a79ded5e08c50b8be
Felimartinez1 <felimartinez072@gmail.com>
2024-08-23 17:19:11 -0300

    fix lang validation

--- Phonemixv2/src/lang_validation.py
+++ Phonemixv2/src/lang_validation.py
@@ -8,16 +8,17 @@ def validate_language(text, expected_language):
         return False, "Could not detect the language of the text."
     
     languages = phonemize_config['lang_validation']
-    if not expected_language == None:
-        expected_language = expected_language.lower()
     
-    if expected_language not in languages.values():
+    # Verifica si el idioma esperado está en el mapeo
+    if expected_language not in [code for codes in languages.values() for code in codes]:
         return False, "Unsupported language."
 
+    # Verifica si el idioma detectado está en el mapeo
     if detected_language not in languages:
         return False, f"Detected language not supported: {detected_language}"
 
-    if languages[detected_language] == expected_language:
+    # Compara si el idioma detectado es uno de los esperados
+    if expected_language in languages[detected_language]:
         return True, "The text matches the expected language."
     else:
-        return False, f"The text is in {languages[detected_language]}, but {expected_language} was expected."
+        return False, f"The text is in {languages[detected_language][0]}, but {expected_language} was expected."
