5d1fa9dff06ff7e24f9ba9db6e36e10d82f5f5f7
Felimartinez1 <felimartinez072@gmail.com>
2024-08-24 20:02:11 -0300

    fix logo path

--- Phonemixv2/app.py
+++ Phonemixv2/app.py
@@ -49,7 +49,7 @@ def pronunciation_feedback(language, expected_text, file_path):
 # Crear la interfaz de Gradio
 with gr.Blocks() as demo:
     with gr.Row():
-        gr.Image("static/logo.png", elem_id="logo", width=50, height=50)  # Agrega el logo
+        gr.Image("static/logo.jpg", elem_id="logo", width=50, height=50)  # Agrega el logo
         gr.Markdown("# Phonemix: Pronunciation Feedback Tool")  # Agrega el título
 
     text_input = gr.State("")
