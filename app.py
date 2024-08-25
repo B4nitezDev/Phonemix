a6225c6f1075538695017571fe8c7eb408dc89ec
b4nitezdev <benitezdev101@gmail.com>
2024-08-25 15:01:20 -0300

    fect: (image) genero la image del deploy

--- app.py
+++ app.py
@@ -103,4 +103,4 @@ with gr.Blocks() as demo:
         ]
     )
 
-demo.launch(server_name="phonemix-production.up.railway.app", server_port=8000)
+demo.launch(server_name="0.0.0.0", server_port=7860)
